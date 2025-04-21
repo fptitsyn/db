17b3eafafaf1256677249e6281c55bd8df95a70c
Niston <vadimpavlov@bk.ru>
2025-04-21 23:50:29 +0300

    Доработать roles_grants.sql

--- apprsc/src/main/resources/roles_grants.sql
+++ apprsc/src/main/resources/roles_grants.sql
@@ -40,6 +40,32 @@ CREATE ROLE "ANALYSTS" WITH
 	CONNECTION LIMIT -1;
 COMMENT ON ROLE "ANALYSTS" IS 'Аналитики имеют доступ ко всем таблицам на чтение, кроме Администрирования';
 --Добавить гранты
+
+GRANT SELECT ON TABLE public.bonus_account TO "ANALYSTS";
+GRANT SELECT ON TABLE public.bonus_account_operation TO "ANALYSTS";
+GRANT SELECT ON TABLE public.category_of_component TO "ANALYSTS";
+GRANT SELECT ON TABLE public.client_status TO "ANALYSTS";
+GRANT SELECT ON TABLE public.clients TO "ANALYSTS";
+GRANT SELECT ON TABLE public.component TO "ANALYSTS";
+GRANT SELECT ON TABLE public.employee_service TO "ANALYSTS";
+GRANT SELECT ON TABLE public.employees TO "ANALYSTS";
+GRANT SELECT ON TABLE public.employees_moving TO "ANALYSTS";
+GRANT SELECT ON TABLE public.inventory TO "ANALYSTS";
+GRANT SELECT ON TABLE public.inventory_issue TO "ANALYSTS";
+GRANT SELECT ON TABLE public.inventory_receipt TO "ANALYSTS";
+GRANT SELECT ON TABLE public.invoice_for_payment TO "ANALYSTS";
+GRANT SELECT ON TABLE public.locations TO "ANALYSTS";
+GRANT SELECT ON TABLE public.locations_type TO "ANALYSTS";
+GRANT SELECT ON TABLE public.order_components TO "ANALYSTS";
+GRANT SELECT ON TABLE public.order_services TO "ANALYSTS";
+GRANT SELECT ON TABLE public.order_status TO "ANALYSTS";
+GRANT SELECT ON TABLE public.orders TO "ANALYSTS";
+GRANT SELECT ON TABLE public.schedule TO "ANALYSTS";
+GRANT SELECT ON TABLE public.services TO "ANALYSTS";
+GRANT SELECT ON TABLE public.staffing_table TO "ANALYSTS";
+GRANT SELECT ON TABLE public.type_of_device TO "ANALYSTS";
+GRANT SELECT ON TABLE public.work_order_status TO "ANALYSTS";
+GRANT SELECT ON TABLE public.work_orders TO "ANALYSTS";
 -------------------------------------------------------------------------------------------------------------------------------------------
 -- Role: "SALES"
 CREATE ROLE "SALES" WITH
@@ -53,4 +79,13 @@ CREATE ROLE "SALES" WITH
 	CONNECTION LIMIT -1;
 COMMENT ON ROLE "SALES" IS 'Менеджеры имеют доступы на создание, изменение, просмотр к клиентам, заказам, нарядам, офисам (просмотр)';
 --Добавить гранты
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.clients TO "SALES";
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.orders TO "SALES";
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.order_services TO "SALES";
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.order_components TO "SALES";
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.order_status TO "SALES";
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.work_orders TO "SALES";
+GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE public.work_order_status TO "SALES";
+GRANT SELECT ON TABLE public.locations TO "SALES";
+GRANT SELECT ON TABLE public.locations_type TO "SALES";
 -------------------------------------------------------------------------------------------------------------------------------------------
